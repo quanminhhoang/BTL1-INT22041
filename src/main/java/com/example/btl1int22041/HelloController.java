86fb58e68ea1ed6deb292d0f9bef4fd35b6a4601
Quan <80340533+quanminhhoang@users.noreply.github.com>
2021-09-28 17:59:32 +0700

    Initial commit

---
+++ src/main/java/com/example/btl1int22041/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.btl1int22041;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.btl1int22041 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.btl1int22041 to javafx.fxml;
+    exports com.example.btl1int22041;
+}
