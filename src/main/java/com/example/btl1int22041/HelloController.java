ec72626aa7fc134d5d3b8f967f3c8880177801ea
Quan <80340533+quanminhhoang@users.noreply.github.com>
2021-09-28 20:55:53 +0700

    ui

--- src/main/java/com/example/btl1int22041/HelloController.java
+++ src/main/java/com/example/btl1int22041/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.btl1int22041;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    static int count = 0;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField tfInput;
+
+    @FXML
+    public Button btSearch;
+
+    @FXML
+    public TextArea taMeaning;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        taMeaning.setText("xin chao");
+        tfInput.setText("nhap tu");
+        btSearch.setOnMouseClicked(event -> {
+            count++;
+            taMeaning.setText("vua nhap "+count+" lan");
+        });
     }
 }
