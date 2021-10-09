dfc63dd6d61764931e61d1b98af07056c6765d5a
Quan <80340533+quanminhhoang@users.noreply.github.com>
2021-10-09 14:01:50 +0700

    TTS

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.dictionaryapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires jsapi;
 
 
     opens com.example.dictionaryapp to javafx.fxml;
